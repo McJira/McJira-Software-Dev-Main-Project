ddd07f6776f0048201c723714a9ef798b307a976
Mikul <mshap08@gmail.com>
2024-10-04 14:18:41 -0400

    This Commit adds some basic methods/class/variables that we may of may not use. It just thought it would be helpful for everyone to start with some common things instead of having everyone code on there own.

--- McJira-Software-Dev-Main-Project/Header.h
+++ McJira-Software-Dev-Main-Project/Header.h
@@ -1,1 +1,47 @@
 #pragma once
+#ifndef DUNGEON_GAME_H
+#define DUNGEON_GAME_H
+
+class Game {
+public:
+    void start();
+    void renderRoom();
+    void changeRoom();
+    void combat();
+};
+
+class Player {
+public:
+    void attackEnemy();
+    void takeDamage();
+    void collectItem();
+};
+
+class Room {
+public:
+    void enter();
+};
+
+class Enemy {
+public:
+    void takeDamage();
+    void attackPlayer();
+};
+
+class Item {
+public:
+    void use();
+};
+
+class Trap {
+public:
+    void trigger();
+};
+
+class Inventory {
+public:
+    void addItem();
+    void removeItem();
+};
+
+#endif // DUNGEON_GAME_H
--- McJira-Software-Dev-Main-Project/Source.cpp
+++ McJira-Software-Dev-Main-Project/Source.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include "dungeon_game.h"
 using namespace std;
 
 int main() {
 	cout << "Hello World this is Michael!";
 	return 0;
 }
+
+// Game class
+void Game::start() {}
+void Game::renderRoom() {}
+void Game::changeRoom() {}
+void Game::combat() {}
+
+// Player class
+void Player::attackEnemy() {}
+void Player::takeDamage() {}
+void Player::collectItem() {}
+
+// Room class
+void Room::enter() {}
+
+// Enemy class
+void Enemy::takeDamage() {}
+void Enemy::attackPlayer() {}
+
+// Item class
+void Item::use() {}
+
+// Trap class
+void Trap::trigger() {}
+
+// Inventory class
+void Inventory::addItem() {}
+void Inventory::removeItem() {}
