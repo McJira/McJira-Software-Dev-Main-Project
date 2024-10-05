cdedae544f45d6e453597a5f507cdd61e7403c77
Itamar1567 <161668143+Itamar1567@users.noreply.github.com>
2024-10-05 13:47:07 -0400

    Added Player class with declaration,  Added Player cpp with definitions

---
+++ McJira-Software-Dev-Main-Project/Player.cpp
@@ -0,0 +1,47 @@
+
+// Player class
+#include "Player.h"
+
+
+//void Player::takeDamage() {}
+//void Player::collectItem() {}
+
+
+//Sets initial health to max health when game starts, to be called in constructor
+void Player::setInitialHealth()
+{
+	currentHealth = maxHealth;
+}
+
+//funtion to decrease health based on a damage parameter
+void Player::takeDamage(int damage)
+{
+
+	currentHealth -= damage;
+
+}
+
+//Returns the current health of the player
+int Player::getHealth() const
+{
+	return currentHealth;
+}
+
+//initializes the player
+Player::Player()
+{
+
+	Player::setInitialHealth();
+
+};
+
+//destroys player object instance
+Player::~Player()
+{
+
+	cout << "Player object destroyed.";
+
+}
+
+
+
