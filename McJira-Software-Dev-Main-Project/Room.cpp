48bc588d59062c6605982dc6d3b79480fd05884d
Itamar1567 <Itamarbaharav@gmail.com>
2024-10-14 23:54:43 -0400

    Added Commentation for the room class and its functionalities

--- McJira-Software-Dev-Main-Project/Room.cpp
+++ McJira-Software-Dev-Main-Project/Room.cpp
@@ -1,19 +1,23 @@
 #include "Room.h"
 
 
+
 void Room::setRoom(int roomNumber)
 {
+	//sets the room variable with a given room input
 	room = roomNumber;
 }
 
 void Room::setRoomSide(string side)
 {
+	//sets the roomSide variable with a given room side input
 	roomSide = side;
 }
 
 void Room::getLocation()
 {
 
+	//retrieves the location of the player
 
 	cout << "You are now in room: " << getRoom() << " on side " << getRoomSide() << endl;
 
@@ -22,7 +26,7 @@ void Room::getLocation()
 }
 int Room::getRoom()
 {
-
+	//retrieves the current room
 	return room;
 
 }
@@ -30,6 +34,7 @@ int Room::getRoom()
 string Room::getRoomSide()
 {
 
+	//retrieves the current room side
 	return roomSide;
 
 }
@@ -38,6 +43,7 @@ string Room::getRoomSide()
 Room::Room()
 {
 
+	//Constructs the spawn location of the player
 	setRoom(1);
 	setRoomSide("North");
 
--- McJira-Software-Dev-Main-Project/Room.h
+++ McJira-Software-Dev-Main-Project/Room.h
@@ -2,16 +2,40 @@
 #include <iostream>
 using namespace std;
 
+
+
+/*
+
+jobs of the room class:
+
+#1. Create rooms
+	*discriptions to be incorporated from seperate classes
+	
+#2. Contains the location of the player
+
+#3. To be used together with either a movement class or the player class
+
+*/
+
+
+
 class Room
 {
 
+
 public:
 
+	//function to return current room the player is in
 	int getRoom();
+	//function to return current room side the player is in
 	string getRoomSide();
+	//function to set room
 	void setRoom(int);
+	//function to set room side
 	void setRoomSide(string);
+	//function to return the current location of the player
 	void getLocation();
+	//Constructs to set the spawn room and room side
 	Room();
 
 
@@ -20,7 +44,9 @@ public:
 
 private:
 
+	//room var to set what room the player is in or moves to
 	int room;
+	//room side var to set what side of the room the player is in or moves to
 	string roomSide;
 
 
