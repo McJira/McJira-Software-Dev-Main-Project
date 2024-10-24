4abd86c91f216885a783de330a9f63acd4865e31
jazangara <j.zangara@student.fdu.edu>
2024-10-23 20:59:51 -0400

    Added get room desc function, changed room funct so that each room automatically comes with a desc and so you can more easily access it using displayroominfo funct

--- McJira-Software-Dev-Main-Project/Room.h
+++ McJira-Software-Dev-Main-Project/Room.h
@@ -11,29 +11,35 @@ using namespace std;
 class Room {
 private:
     string RoomName;//name of the room
-    
+    string RoomDescription;
 
 public:
     
-    Room() : RoomName("Unnamed") {}//default constructor
+    Room() : RoomName("Unnamed"), RoomDescription("Unavailable") {} //default constructors
 
+    Room(const string& name, const string& description)
+        : RoomName(name), RoomDescription(description){}    //constructor to initialize the room with specific values
 
-    Room(const string& name): RoomName(name){}    //constructor to initialize the room with specific values
-
-
-    string GetRoomName() const {//getter for RoomName
+    string GetRoomName() const { //getter for RoomName
         return RoomName;
     }
 
 
-    void SetRoomName(const string& name) {//setter for RoomName
+    void SetRoomName(const string& name) { //setter for RoomName
         RoomName = name;
     }
 
+    string GetRoomDescription() const { // getter for RoomDesc
+        return RoomDescription;
+    }
+
+    void SetRoomDescription(const string& description) { // setter for RoomDesc
+        RoomDescription = description;
+    }
 
     
     void DisplayRoomInfo() const {//method to display room information
         cout << "Room Name: " << RoomName << endl;
-
+        cout << "Room Description: " << RoomDescription << endl;
     }
 };
--- McJira-Software-Dev-Main-Project/map.h
+++ McJira-Software-Dev-Main-Project/map.h
@@ -13,10 +13,10 @@ public:
     //constructor to create the map with 4 rooms
     Map(){
         //creating the 4 rooms. (more will be added later)
-        dungeon[0][0] = Room("Entrance");
-        dungeon[0][1] = Room("Hallway");
-        dungeon[1][0] = Room("Armory");
-        dungeon[1][1] = Room("Treasure Room");
+        dungeon[0][0] = Room("Entrance", "You are in the eastern side of the lobby, locked doors are infront of you, a sign above says Advising Office\n");
+        dungeon[0][1] = Room("Hallway", "You are in the northern side of the lobby, a long hallway is infront of you, to your left a door lay open, a key positioned under it\n");
+        dungeon[1][0] = Room("Armory", "You are in the southern side of the lobby, infront of you is a broken stairway, going up, under it lays another stairway going down, locked by gate, a key hole situated inside\n");
+        dungeon[1][1] = Room("Treasure Room", "You are in the western side of the lobby, double doors lay behind you, to your right lay a couch and a table\n");
     }
 
     //function to convert a string to lowercase so that the case does not matte  
@@ -24,6 +24,7 @@ public:
     void DisplayMap(int,int) const;
     void GetRoomDescription(int x, int y) const
     {
+        cout << endl;
         return dungeon[x][y].DisplayRoomInfo();
     }
 };
